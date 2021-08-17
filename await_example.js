eacef9df08e3a5eb7b13dd9fc0ba9f989234bb77
David Wu <dwu@sonicwall.com>
2021-08-17 11:11:51 +0800

    add wrapper function

--- await_example.js
+++ await_example.js
@@ -1,15 +1,22 @@
-
-
 async function sleep(time, msg, error) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log(msg);
-      if(error) {
-          reject(time);
+      if (error) {
+        reject(time);
+      } else {
+        resolve(time);
       }
-      resolve(time);
     }, time);
   });
+}
+
+async function wrapper(time, msg, error) {
+    try {
+        return await sleep(time, msg, error)
+    }catch(e) {
+        console.log(`wrapper error: ${e}`);
+    }
 }
 
 async function createOrUpdateLicenses() {
@@ -18,7 +25,7 @@ async function createOrUpdateLicenses() {
   console.log(`return value for the first try: ${firstTry}`);
 
   try {
-    sleep(1000, "sencond try to wait 1s to print this", false);
+    wrapper(1000, "sencond try to wait 1s to print this", true);
   } catch (e) {
     console.log(`error in the second try ${e}`);
   }
@@ -33,3 +40,15 @@ createOrUpdateLicenses().then((res) => {
     `called function createOrUpdateLicenses with return value ${res}`
   );
 });
+
+
+// createOrUpdateLicenses().then((res) => {
+//     console.log(
+//       `called function createOrUpdateLicenses with return value ${res}`
+//     );
+//   });
+  
+
+// sleep(20000, "wait 2s").then((res) => {
+//   console.log(res);
+// });
